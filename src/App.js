bfc7624d2412665c0ec3f7133bec1bbd31eb3751
Animesh239 <animeshbarik239@gmail.com>
2024-08-12 18:17:35 +0530

    navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from "react";
 import "./index.css";
 import Carousel from "./components/carousel";
+import Navbar from "./components/navbar";
 
 const App = () => {
   return (
+    <>
+      <Navbar/>
     <div className="min-h-screen bg-gradient-to-r from-gray-900 to-black flex items-center justify-center">
       <Carousel />
     </div>
+    </>
   );
 };
 
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,7 +1,7 @@
 const Navbar = () =>{
     return (
-        <div className="flex justify-end w-full h-8">
-            <button className="px-4 py-2 bg-cyan-400 text-black rounded-lg">
+        <div className="flex absolute justify-end w-full my-6 h-8">
+            <button className="w-40 mx-10 px-4 bg-cyan-400 font-bold text-black rounded-lg">
                 Admin Login
             </button>
         </div>
